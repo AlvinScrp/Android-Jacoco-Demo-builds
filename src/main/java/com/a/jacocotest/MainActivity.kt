9359768528c88cb780af83f34abf548ef906a40c
canglong <351033166@qq.com>
2023-03-03 11:17:45 +0800

    'commit:b3'

---
+++ src/main/java/com/a/jacocotest/MainActivity.kt
@@ -0,0 +1,91 @@
+package com.a.jacocotest
+
+import android.content.Intent
+import android.content.pm.PackageManager.GET_META_DATA
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View.OnClickListener
+import com.a.jacocotest.databinding.ActivityMainBinding
+import com.a.other.OtherActivity
+import com.a.privacy_sample.PrivacyVisitor
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        binding.btn1.setOnClickListener { valueClick.onOkClick2() }
+        binding.btn2.setOnClickListener { valueClick.onBadClick()  }
+        binding.btnStartComposeActivity.setOnClickListener {
+//            startActivity(Intent(this@MainActivity,ComposeActivity::class.java))
+        }
+        binding.btnStartOtherActivity.setOnClickListener {
+            startActivity(Intent(this@MainActivity,OtherActivity::class.java))
+        }
+
+        binding.btnApkMd5.setOnClickListener (listener)
+        binding.btnBuildNum.setOnClickListener {
+            getMetaBuildNum2()
+        }
+        binding.btnGenJacoco.setOnClickListener { JacocoHelper.generateCoverageFile(this@MainActivity) }
+
+        Log.d("alvin","oncreate")
+    }
+
+    private fun getMetaBuildNum2() {
+        val context = this
+        val appInfo = context.packageManager.getApplicationInfo(context.packageName, GET_META_DATA);
+        val buildNum = appInfo.metaData.getInt("JENKINS_BUILD_NUM");
+        binding.tv.text = "buildNum:${buildNum}"
+
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("alvin","onResume")
+    }
+
+
+    private fun updateText(ok: Boolean) {
+        val text = if (ok) {
+            getOKText()
+        } else {
+            getBadCode()
+        }
+        binding.tv.text = text
+    }
+
+    private fun getOKText(): String = "OK!!!!"
+
+    private fun getBadCode(): String {
+        return "Bad!"
+    }
+
+    val listener = OnClickListener {
+        ApkUtil.getApkMD5(this@MainActivity)
+        PrivacyVisitor.visitPrivacy(this@MainActivity)
+    }
+    val valueClick = object :OnEventListener{
+        override fun onOkClick2() {
+            updateText(true)
+        }
+
+        override fun onBadClick() {
+            updateText(false)
+        }
+
+    }
+
+    interface OnEventListener {
+        fun onOkClick2()
+        fun onBadClick()
+    }
+
+}
